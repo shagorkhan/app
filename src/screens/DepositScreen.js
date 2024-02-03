d2523c39c1cd1f344bfa25dd07fe55152ddbface
shagorkhan <42969481+shagorkhan@users.noreply.github.com>
2024-02-03 16:20:17 +0600

    no

--- src/screens/DepositScreen.js
+++ src/screens/DepositScreen.js
@@ -1,17 +1,27 @@
+//import { forHorizontalIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 
 const DepositScreen = () => {
   const [selectedPayment, setSelectedPayment] = useState(null);
+  const [selectedAmount, setSelectedAmount] = useState(null);
 
   const handlePaymentSelection = (paymentMethod) => {
     setSelectedPayment(paymentMethod);
     // Add logic to handle the selected payment method (e.g., navigate to the next screen)
   };
+  const handleAmountSelection = (amount) => {
+    setSelectedAmount(amount);
+  };
 
   return (
     <View style={styles.container}>
+      <Text style={{fontSize:40, textAlign:'center', marginBottom:20}}>Deposit</Text>
+      <Text style={{fontSize:15, textAlign:'center'}}>With your tasted wallet and explore more</Text>
+      <Text style={{fontSize:15, textAlign:'center', marginBottom:40}}>gaming features with our  client area</Text>
       <Text style={styles.title}>Select Payment Method</Text>
+      <View>
+      <ScrollView horizontal={true}>
       <TouchableOpacity
         style={[styles.paymentOption, selectedPayment === 'bKash' && styles.selectedPayment]}
         onPress={() => handlePaymentSelection('bKash')}
@@ -39,6 +49,63 @@ const DepositScreen = () => {
       >
         <Text style={styles.paymentText}>UPay</Text>
       </TouchableOpacity>
+      </ScrollView>
+      </View>
+      <Text style={{fontSize:15}}>Deposit Channel</Text>
+      <TouchableOpacity
+        style={[styles.paymentOption, selectedPayment === 'Dpay' && styles.selectedPayment]}
+       // onPress={() => handlePaymentSelection('Dpay')}
+      >
+        <Text style={styles.paymentText}>Dpay</Text>
+      </TouchableOpacity>
+      <Text style={{fontSize:15}}>Amount  200 Taka- 25,000 Taka </Text>
+      <View style={styles.amountContainer}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.amountOption, selectedAmount === 200 && styles.selectedAmount]}
+            onPress={() => handleAmountSelection(200)}
+          >
+            <Text style={styles.amountText}>200</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.amountOption, selectedAmount === 500 && styles.selectedAmount]}
+            onPress={() => handleAmountSelection(500)}
+          >
+            <Text style={styles.amountText}>500</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.amountOption, selectedAmount === 1000 && styles.selectedAmount]}
+            onPress={() => handleAmountSelection(1000)}
+          >
+            <Text style={styles.amountText}>1,000</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.amountOption, selectedAmount === 2000 && styles.selectedAmount]}
+            onPress={() => handleAmountSelection(2000)}
+          >
+            <Text style={styles.amountText}>5,000</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.amountOption, selectedAmount === 5000 && styles.selectedAmount]}
+            onPress={() => handleAmountSelection(5000)}
+          >
+            <Text style={styles.amountText}>10,000</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.amountOption, selectedAmount === 10000 && styles.selectedAmount]}
+            onPress={() => handleAmountSelection(10000)}
+          >
+            <Text style={styles.amountText}>20,000</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Text style={{fontSize:15}}>Deposit Bonus </Text>
+
     </View>
   );
 };
@@ -46,19 +113,21 @@ const DepositScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 10,
+    //justifyContent: 'center',
+    //alignItems: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 14,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 5,
   },
   paymentOption: {
     backgroundColor: '#e0e0e0',
-    padding: 20,
-    marginVertical: 10,
-    width: 200,
+    padding: 10,
+    marginHorizontal: 5,
+    width: 80,
+    height: 50,
     alignItems: 'center',
     borderRadius: 10,
   },
@@ -68,6 +137,31 @@ const styles = StyleSheet.create({
   },
   selectedPayment: {
     backgroundColor: 'blue', // Change the color for the selected payment method
+    borderColor: 'white',
+  },
+  amountContainer: {
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+    flexWrap:"wrap",
+  },
+  amountOption: {
+    backgroundColor: '#e0e0e0',
+    padding: 10,
+    marginVertical: 5,
+    width: 100,
+    alignItems: 'center',
+    borderRadius: 7,
+  },
+  amountText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  selectedAmount: {
+    backgroundColor: 'blue',
     borderColor: 'white',
   },
 });
