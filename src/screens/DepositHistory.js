6716b1568b76c84efe120de74b81031a6195abbd
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-02-07 10:07:04 +0600

    oik

--- src/screens/DepositHistory.js
+++ src/screens/DepositHistory.js
@@ -3,14 +3,25 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Input from '../components/Input';
 import Depositewithdrawcard from '../components/DepositeWithdrawCard';
+import img1 from "../../assets/image12.png";
+
 
 const DepositHistory = () => {
   return (
     <View style={styles.container}>
       <Input placeholder="Search..."></Input>
       <View style={styles.cardContainer}>      
-        <Depositewithdrawcard></Depositewithdrawcard>
-        <Depositewithdrawcard></Depositewithdrawcard>
+        <Depositewithdrawcard  
+        tnxId='dhjgwycikj87'
+        status='CANCELED'
+        amount='500'
+        offer="100"
+        remark="Thank you"
+        date="12 Jan 2000"
+        icon={img1}
+        walletName="Bkash"
+        ></Depositewithdrawcard>
+        
       </View>
     </View>
   );
