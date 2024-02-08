1e4b5c7d634e595ac41b525fb643fa0f4f01d224
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-02-08 17:00:58 +0600

    ok

--- src/screens/TurnoverPending.js
+++ src/screens/TurnoverPending.js
@@ -1,10 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { useLoader } from "../context/LoaderContext";
+import { getValue } from "../functions/storage";
+import { StatusBar } from "expo-status-bar";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import DepositBonusCard from "../components/RewardBonusCard";
+import BackButton from "../components/BackButton";
+import getRewards from "../apis/getRewards";
+import TurnoverHistoryPendingCard from "../components/TurnoverHistoryPendingCard";
 
-export default function TurnoverPending() {
+const TurnoverPending = () => {
+  const { showLoader, hideLoader } = useLoader();
+  const [data, setData] = useState([]);
+  const inset = useSafeAreaInsets();
+
+  // useEffect(() => {
+  //   const loadRewards = async () => {
+
+  //     try {
+  //       const token = await getValue("token");
+  //       showLoader();
+  //       if (!token) {
+  //         hideLoader();
+  //         return navigation.goBack();
+  //       }
+  //       const res = await getRewards(token);
+  //       setData(res.data);
+  //       hideLoader();
+  //     } catch (error) {
+  //       hideLoader();
+  //       console.error(error);
+  //     }
+  //   };
+  //  loadRewards();
+  // }, []);
+
   return (
-    <View>
-      <Text>TurnoverPending</Text>
+    <View style={[styles.container]}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.cardContainer}
+      >
+        <TurnoverHistoryPendingCard
+          title="5% deposit bonus and you will win fkhjkbvuyi dhvbcduh the gam..."
+          targetAmount="500"
+          completedAmount="350"
+          date="7 Jan, 2023"
+        ></TurnoverHistoryPendingCard>
+        <TurnoverHistoryPendingCard
+          title="5% deposit bonus and you will win fkhjkbvuyi dhvbcduh the gam..."
+          targetAmount="500"
+          completedAmount="350"
+          date="7 Jan, 2023"
+        ></TurnoverHistoryPendingCard>
+        <TurnoverHistoryPendingCard
+          title="5% deposit bonus and you will win fkhjkbvuyi dhvbcduh the gam..."
+          targetAmount="500"
+          completedAmount="350"
+          date="7 Jan, 2023"
+        ></TurnoverHistoryPendingCard>
+        <TurnoverHistoryPendingCard
+          title="5% deposit bonus and you will win fkhjkbvuyi dhvbcduh the gam..."
+          targetAmount="500"
+          completedAmount="350"
+          date="7 Jan, 2023"
+        ></TurnoverHistoryPendingCard>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  cardContainer: {
+    marginTop: 20,
+  },
+});
+
+export default TurnoverPending;
