b3d11d485ff4c9ffedbe2496a75683808fdd67f8
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-02-08 16:25:59 +0600

    ok

--- src/routes/TurnoverRoutes.js
+++ src/routes/TurnoverRoutes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { View } from "react-native";
-import { StatusBar } from 'expo-status-bar';
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import TurnoverPending from "../screens/TurnoverPending";
 import TurnoverCompleted from "../screens/TurnoverCompleted";
@@ -11,8 +10,7 @@ const Tab = createMaterialTopTabNavigator();
 const TurnoverRoutes = () => {
   const inset=useSafeAreaInsets()
   return (
-    <View style={{flex:1,marginTop:inset?.top}}>
-      <StatusBar style={"dark"} backgroundColor={"#D9D9D9"} />
+    <View style={{flex:1}}>
       <Tab.Navigator>
         <Tab.Screen name="Pending" component={TurnoverPending} />
         <Tab.Screen name="Completed" component={TurnoverCompleted} />
